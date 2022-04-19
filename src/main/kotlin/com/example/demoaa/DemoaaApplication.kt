09c00d58cea7082e457e1fc951aa2356a07db873
jungjaewook <rentalhub@rentalhubui-MacBookPro.local>
2022-04-19 21:21:56 +0900

    first commit

---
+++ src/main/kotlin/com/example/demoaa/DemoaaApplication.kt
@@ -0,0 +1,11 @@
+package com.example.demoaa
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DemoaaApplication
+
+fun main(args: Array<String>) {
+    runApplication<DemoaaApplication>(*args)
+}
---
+++ src/main/kotlin/com/example/demoaa/controller/MainController.kt
@@ -0,0 +1,15 @@
+package com.example.demoaa.controller
+
+import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
+import org.springframework.web.bind.annotation.GetMapping
+
+@Controller
+class MainController {
+
+    @GetMapping
+    fun test(model: Model) : String {
+        model.addAttribute("title", "test");
+        return "index"
+    }
+}
